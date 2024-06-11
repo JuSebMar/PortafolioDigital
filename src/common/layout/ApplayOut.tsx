d43cbe7430833165682cef32d9e13358cb12d6bd
Sebastian Soto <juansebastiansotomartin@hotmail.com>
2024-06-11 17:16:54 -0300

    ??

--- src/common/layout/ApplayOut.tsx
+++ src/common/layout/ApplayOut.tsx
@@ -1,14 +1,13 @@
 import { Navbar } from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./components/Footer";
-import { Box } from "@mui/material";
 
 export const AppLayOut = () => {
   return (
-    <Box sx={{ maxWidth: "100%", position: "relative" }}>
+    <>
       <Navbar />
       <Outlet />
       <Footer />
-    </Box>
+    </>
   );
 };
